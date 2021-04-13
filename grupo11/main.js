3b758fc5dac0ab21bd52835fe29352641d7befbc
Seba <sebastianrodas11@gmail.com>
2021-04-13 16:31:11 -0300

    Impresión del mensaje por pantalla

--- grupo11/main.js
+++ grupo11/main.js
@@ -1,14 +1,21 @@
-var http = require("http");
+let http = require("http");
+let fs = require("fs");
+ 
+let handleRequest = (request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  fs.readFile("./index.html", null, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      respone.write("Whoops! File not found!");
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+};
+ 
+http.createServer(handleRequest).listen(8081);
+console.log("Server running at http://127.0.0.1:8081/");
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
