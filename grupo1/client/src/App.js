33071f9d60464426886ff15f1d6a1dbc3f0df8be
gustavoygarzabal <33422630+gustavoygarzabal@users.noreply.github.com>
2021-04-14 11:45:13 -0300

    Added a new function that sum(a+b)

--- grupo1/client/src/App.js
+++ grupo1/client/src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 
 function App() {
 	const [text, setText] = useState('');
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(0);
 	const onChange = (e) => setText(e.target.value);
+	const onChangeA = (e) => setA(parseInt(e.target.value));
+	const onChangeB = (e) => setB(parseInt(e.target.value));
 	const onSend = () => {
 		alert(text);
+	};
+	const sumar = () => {
+		alert(a+b);
 	};
 	return (
 		<div className="App">
 			<h1>hola mundo</h1>
 			<input placeholder="..." value={text} onChange={onChange} />
 			<button onClick={onSend}>Enviar</button>
+			<input type="number" placeholder="..." value={a} onChange={onChangeA} />
+			<input type="number" placeholder="..." value={b} onChange={onChangeB} />
+			<button onClick={sumar}>Sumar</button>
 		</div>
 	);
 }
