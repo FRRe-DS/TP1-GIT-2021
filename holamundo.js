5e33d42cc6f9975339ac4dedc375cf9aea94158e
MauSanchez222 <mauricioasan96@gmail.com>
2021-04-14 15:15:26 -0300

    Añadida una función de entrada por pantalla

--- holamundo.js
+++ holamundo.js
@@ -3,6 +3,17 @@
 const http = require('http');
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola Mundo');
+    res.end('Hola Mundo\n');
 });
 server.listen(8000);
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Como te llamas? ', nombre =>{
+    console.log(`Hola ${nombre}`);
+    readline.close();
+}
+)
