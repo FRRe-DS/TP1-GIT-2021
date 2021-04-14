ddf79d97225c63f4c5706d6428537830dd6462e5
LeanA23 <arielchanampa12@gmail.com>
2021-04-14 11:54:41 -0300

    Primer proyecto node

---
+++ holamundo.js
@@ -0,0 +1,8 @@
+// server.js
+'use strict';
+const http = require('http');
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hola Mundo');
+});
+server.listen(8000);
