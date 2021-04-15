86d6eaa7b513078de53d42cac81d756edfb5ec8f
hadamafer <hadamafer@gmail.com>
2021-04-15 19:21:33 -0300

    cambio que tira en error en tp1

--- grupo8/tp1.js
+++ grupo8/tp1.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Hello soy Manu\n');
+   respon.end('Hello soy Manu\n');
 }).listen(8081);
 
 // Console will print the message
