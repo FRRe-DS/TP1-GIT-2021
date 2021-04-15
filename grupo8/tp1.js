383a0a6f89a05c25544b60d39e4d4ae83db327a1
martingetzel <martingetzel@gmail.com>
2021-04-15 18:29:05 -0300

    Cambio de Martín

--- grupo8/tp1.js
+++ grupo8/tp1.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello soy fer\n');
+   response.end('Hello soy fer\n y yo soy Martín\n');
 }).listen(8081);
 
 // Console will print the message
