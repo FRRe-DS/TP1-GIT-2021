200582a0047b53f66e11fdca81ef2454455319f9
MauSanchez222 <mauricioasan96@gmail.com>
2021-04-15 20:23:54 -0300

    Conflicto

--- holamundo.js
+++ holamundo.js
@@ -15,4 +15,9 @@ readline.question('Como te llamas? ', nombre =>{
     console.log(Hola ${nombre} que tengas buen dia);
     readline.close();
 }
-)
+);
+
+const prompt = require('prompt-sync')({sigint: true});
+const num = prompt('Enter a number: ');
+console.log('Your number + 4 =');
+console.log(Number(num) + 4);
