c5984d36bd689eb4a107f7aa65a89c30460c7d98
Xircodra <kusui.rukodora@gmail.com>
2021-04-15 19:06:14 -0300

    agregara una funcion

---
+++ grupo10/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function add(x1,x2){
+    return x1+x2;
+}
+
+function substract(x1,x2){
+    return x1-x2;
+}
+
+function multiply(x1,x2){
+    return x1*x2;
+}
+
+function divide(x1,x2){
+    if (x2==0) {
+        console.log('No divisible por cero');
+    } else {
+        return x1/x2;
+    }
+}
