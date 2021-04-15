06f2caadfa235714966bcac75c8076345d7092f6
Xircodra <kusui.rukodora@gmail.com>
2021-04-15 19:13:20 -0300

    2do intento

--- grupo10/index.js
+++ grupo10/index.js
@@ -15,13 +15,7 @@ function add(x1,x2){
     return x1+x2;
 }
 
-function substract(x1,x2){
-    return x1-x2;
-}
 
-function multiply(x1,x2){
-    return x1*x2;
-}
 
 function divide(x1,x2){
     if (x2==0) {
