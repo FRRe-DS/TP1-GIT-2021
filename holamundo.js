424903a3694a1c578a0057c5f51ddc27f02e1852
nicolasdajruch <81260012+nicolasdajruch@users.noreply.github.com>
2021-04-15 17:53:00 -0300

    Update holamundo.js

--- holamundo.js
+++ holamundo.js
@@ -6,14 +6,3 @@ const server = http.createServer(function (req, res) {
     res.end('Hola Mundo\n');
 });
 server.listen(8000);
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question('Como te llamas? ', nombre =>{
-    console.log(`Hola ${nombre} que tengas buen dia`);
-    readline.close();
-}
-)
