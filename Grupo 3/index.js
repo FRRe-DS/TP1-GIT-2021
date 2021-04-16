656df25e7699da548d372f55d6961c30b0cae56c
NoSens29 <ruben7are@gmail.com>
2021-04-16 12:58:44 -0300

    server node andando

--- Grupo 3/index.js
+++ Grupo 3/index.js
@@ -1,12 +1,16 @@
-var http = require("http");
+//var http = require("http");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+app.use(express.static(__dirname + '/public/'));
+
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//})""
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
